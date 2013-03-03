27d8ca5d6d569151c9f52fcb28c37b5d6dd9097b
DerDackel <sjackel@uni-koblenz.de>
2013-03-03 20:52:34 +0100

    Added test cases to haskellFlatt

--- contributions/haskellFlat/src/Main.hs
+++ contributions/haskellFlat/src/Main.hs
@@ -3,18 +3,38 @@ module Main where
 import Company.Data
 import Company.Total
 import Company.Cut
+import Test.HUnit
+import System.Exit
 
-main
- = do
-      -- De-serialize sample company via read
-      txt <- getContents
-      let company = read txt
+sampleCompany =
+ ( "Acme Corporation",
+   [
+     ("Craig", "Redmond", 123456),
+     ("Erik", "Utrecht", 12345),
+     ("Ralf", "Koblenz", 1234),
+     ("Ray", "Redmond", 234567),
+     ("Klaus", "Boston", 23456),
+     ("Karl", "Riga", 2345),
+     ("Joe", "Wifi City", 2344)
+   ]
+ )
 
-      -- Test that show followed by read returns the same company
-      print $ company == read (show company)
+totalTest = 399747.0 ~=? total sampleCompany
 
-      -- Total all salaries
-      print $ total company
+cutTest = 199873.5 ~=? total (cut sampleCompany)
 
-      -- Cut all salaries and total resulting company
-      print $ total (cut company)
+serializationTest = sampleCompany ~=? read (show sampleCompany)
+
+tests =
+  TestList [
+    TestLabel "total" totalTest,
+    TestLabel "cut" cutTest,
+    TestLabel "serialization" serializationTest
+  ]
+
+
+main = do
+ counts <- runTestTT tests
+ if (errors counts > 0 || failures counts > 0)
+   then exitFailure
+   else exitSuccess
