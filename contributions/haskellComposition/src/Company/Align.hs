8fab4eac6873474f82fd8358aa663e79c3a9123f
Ralf Laemmel <rlaemmel@gmail.com>
2013-03-04 14:18:09 +0100

    Improved haskellCompositon. (Removed obsolete files, added comments, normalized further the .cabal file)

--- contributions/haskellComposition/src/Company/Align.hs
+++ contributions/haskellComposition/src/Company/Align.hs
@@ -2,13 +2,16 @@ module Company.Align where
 
 import Company.Data
 
+-- Check that salaries align with company hierarchy
 align :: Company -> Bool
 align (Company _ ds) = and (map (align' Nothing) ds)
   where
+    -- Helper at the department level
     align' :: Maybe Float -> Department -> Bool
     align' v (Department _ m ds es)
       =  maybe True (>getSalary m) v
       && and (map (align' (Just (getSalary m))) ds)
       && and (map ((<getSalary m) . getSalary) es)
+    -- Extract the salary on an employee
     getSalary :: Employee -> Float
     getSalary (Employee _ _ s) = s
