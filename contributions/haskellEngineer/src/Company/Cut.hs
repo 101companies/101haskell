2f6cfcaa3ddf89050df53ac11d43a983216a7b13
Ralf Laemmel <rlaemmel@gmail.com>
2013-05-12 17:06:16 +0200

    Trivia

--- contributions/haskellEngineer/src/Company/Cut.hs
+++ contributions/haskellEngineer/src/Company/Cut.hs
@@ -1,20 +1,20 @@
 {-| The operation of cutting all salaries of all employees in a company in half -}
 
-module Company.Cut (
-  cut
-) where
+module Company.Cut where
 
 import Company.Data
 
 -- | Cut all salaries in a company
 cut :: Company -> Company
 cut (n, es) = (n, cutEmployees es)
+  where
 
--- Cut the salaries of a list of employees
-cutEmployees :: [Employee] -> [Employee]
-cutEmployees [] = []
-cutEmployees (e:es) = cutEmployee e : cutEmployees es
+    -- Cut the salaries of a list of employees
+    cutEmployees :: [Employee] -> [Employee]
+    cutEmployees [] = []
+    cutEmployees (e:es) = cutEmployee e : cutEmployees es
+      where
 
--- Cut the salary of an employee in half
-cutEmployee :: Employee -> Employee
-cutEmployee (n, a, s) = (n, a, s/2)
+        -- Cut the salary of an employee in half
+        cutEmployee :: Employee -> Employee
+        cutEmployee (n, a, s) = (n, a, s/2)
--- contributions/haskellEngineer/src/Company/Total.hs
+++ contributions/haskellEngineer/src/Company/Total.hs
@@ -1,25 +1,25 @@
 {-| The operation of totaling all salaries of all employees in a company -}
 
-module Company.Total (
-  total,
-  salaries
-) where
+module Company.Total where
 
 import Company.Data
 
 -- | Total all salaries in a company
 total :: Company -> Float
 total = sum . salaries
+  where
 
--- | Extract all salaries in a company
-salaries :: Company -> [Salary]
-salaries (_, es) = getSalaries es
+    -- | Extract all salaries in a company
+    salaries :: Company -> [Salary]
+    salaries (_, es) = getSalaries es
+      where
 
--- Extract all salaries of lists of employees
-getSalaries :: [Employee] -> [Salary]
-getSalaries [] = []
-getSalaries (e:es) = getSalary e : getSalaries es
+        -- Extract all salaries of lists of employees
+        getSalaries :: [Employee] -> [Salary]
+        getSalaries [] = []
+        getSalaries (e:es) = getSalary e : getSalaries es
+          where
 
--- Extract the salary from an employee
-getSalary :: Employee -> Salary
-getSalary (_, _, s) = s
+            -- Extract the salary from an employee
+            getSalary :: Employee -> Salary
+            getSalary (_, _, s) = s
